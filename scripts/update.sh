4bf1a708bff89587ad4e76c1c26bbb46cb492dcb
Weston Steimel <author@code.w.steimel.me.uk>
2025-02-28 22:57:24 +0000

    chore: update to latest patterns
    Signed-off-by: Weston Steimel <author@code.w.steimel.me.uk>

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-set -e
+set -xeuo pipefail
 
-version=`curl --silent "https://api.github.com/repos/keepassxreboot/keepassxc/releases/latest" | jq .tag_name | xargs`
-revision=`curl --silent "https://api.github.com/repos/keepassxreboot/keepassxc/commits/${version}" | jq .sha | xargs`
-version=${version#"v"}
+latest_stable_release_tag=$(curl --silent "https://api.github.com/repos/keepassxreboot/keepassxc/releases/latest" | jq -er .tag_name)
+revision=$(curl --silent "https://api.github.com/repos/keepassxreboot/keepassxc/commits/${latest_stable_release_tag}" | jq -er .sha)
+version=${latest_stable_release_tag#"v"}
 echo "latest stable version: ${version}, revision: ${revision}"
 
 sed -ri \
-    -e 's/^(ARG VERSION=).*/\1'"\"${version}\""'/' \
-    -e 's/^(ARG REVISION=).*/\1'"\"${revision}\""'/' \
+    -e 's/^(ARG KEEPASSXC_BRANCH=).*/\1'"\"${latest_stable_release_tag}\""'/' \
+    -e 's/^(ARG KEEPASSXC_VERSION=).*/\1'"\"${version}\""'/' \
+    -e 's/^(ARG KEEPASSXC_COMMIT=).*/\1'"\"${revision}\""'/' \
     "stable/Dockerfile"
 
 git add stable/Dockerfile
 git diff-index --quiet HEAD || git commit --message "updated stable to version ${version}, revision: ${revision}"
 
-set -e
-
 version="develop"
-revision=`curl --silent "https://api.github.com/repos/keepassxreboot/keepassxc/commits/${version}" | jq .sha | xargs`
+revision=$(curl --silent "https://api.github.com/repos/keepassxreboot/keepassxc/commits/${version}" | jq -er .sha)
 echo "latest edge version: ${version}, revision: ${revision}"
 
 sed -ri \
-    -e 's/^(ARG VERSION=).*/\1'"\"${version}\""'/' \
-    -e 's/^(ARG REVISION=).*/\1'"\"${revision}\""'/' \
+    -e 's/^(ARG KEEPASSXC_BRANCH=).*/\1'"\"${version}\""'/' \
+    -e 's/^(ARG KEEPASSXC_COMMIT=).*/\1'"\"${revision}\""'/' \
     "edge/Dockerfile"
 
 git add edge/Dockerfile
 git diff-index --quiet HEAD || git commit --message "updated edge to version ${version}, revision: ${revision}"
+
